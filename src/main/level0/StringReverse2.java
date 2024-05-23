643935b5ca6b58bd39b9778c2bfd1f8023c49dff
yunrap <dbsdpwl7@gmail.com>
2024-05-24 00:11:10 +0900

    feat. 문자열뒤집기 재수정

--- src/main/level0/StringReverse2.java
+++ src/main/level0/StringReverse2.java
@@ -4,20 +4,24 @@ public class StringReverse2 {
         StringBuilder answerReverse = new StringBuilder();
 
         for (int i = 0; i < s; i++) {
-            answer.append(my_string.charAt(s));
+            answer.append(my_string.charAt(i));
         }
 
-        for (int i = s; i < e; i++) {
-            answerReverse.append(my_string.charAt(i));
+        for (int j = s; j <= e; j++) {
+            answerReverse.append(my_string.charAt(j));
         }
         answerReverse.reverse();
 
         answer.append(answerReverse);
 
-        for (int j = e; j < my_string.length(); j++) {
-            answer.append(my_string.charAt(j));
+        for (int v = e + 1; v < my_string.length(); v++) {
+            answer.append(my_string.charAt(v));
         }
 
         return answer.toString();
     }
 }
+
+// 문제URL : https://school.programmers.co.kr/learn/courses/30/lessons/181905
+// :: 문자열 뒤집기
+// :: 걸린시간 13분 근데 이렇게 직관적으로 소스코드를 해도될까 해서 찾아봄 ... 에구 substring 써먹어자 소스코드가 예뻐지네
